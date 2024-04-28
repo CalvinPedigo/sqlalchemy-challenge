9aa912148626cdc67814c44ddd2ff282ca180591
CalvinPedigo <159497880+CalvinPedigo@users.noreply.github.com>
2024-04-28 15:25:17 -0600

    set up personal side and did some of climate starter

---
+++ Surfsup/app.py
@@ -0,0 +1,37 @@
+# Import the dependencies.
+import pandas as pd
+import numpy as np
+from flask import Flask, jsonify
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from datetime import datetime as dt
+
+
+#################################################
+# Database Setup
+#################################################
+
+
+# reflect an existing database into a new model
+
+# reflect the tables
+
+
+# Save references to each table
+
+
+# Create our session (link) from Python to the DB
+
+
+#################################################
+# Flask Setup
+#################################################
+
+
+
+
+#################################################
+# Flask Routes
+#################################################
